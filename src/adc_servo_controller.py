743762484d4a8688e9619c26906a0a49bf8daddd
Yong Hao <ongyh.21@ichat.sp.edu.sg>
2022-05-19 21:55:15 +0800

    Finish servo code i think

--- src/adc_servo_controller.py
+++ src/adc_servo_controller.py
@@ -1,9 +1,23 @@
-#from time import sleep
+from time import sleep
+from threading import Thread
+from hal import hal_adc as adc
+from hal import hal_servo as servo
 
 
-#from hal import hal_adc as adc
-#from hal import hal_servo as servo
+def adc_servo_init():
+    global adc_value
+    t1 = Thread(target=adc_servo_thread)
+    t1.start()
 
 
-def led_control_init():
-    print("hello")
+def adc_servo_thread():
+    global adc_value
+    while True:
+        adc_value = adc.get_adc_value(1)  #channel 1 = potentialmeter & [probably returns 0 - 1023 vlaue]
+
+
+def check_adc_servo():
+    global adc_value
+    if adc_value <= 1023:
+        percentage = (100 - ((int(adc_value) / 1023) * 100))
+        servo.set_servo_position((percentage/100)*180)
